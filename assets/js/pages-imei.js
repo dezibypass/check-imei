e699dc6878c976cc3bc9b211fa1bd4e37e2a5c61
dezibypass <87120203+dezibypass@users.noreply.github.com>
2023-05-25 01:13:53 +0700

    Update pages-imei.js

--- assets/js/pages-imei.js
+++ assets/js/pages-imei.js
@@ -1,7 +1,7 @@
 $('#formCheckIMEI').submit(function (e) {
 e.preventDefault();
       $.ajax({
-         url: "https://long-term-groom.000webhostapp.com/cek.php",
+         url: "https://api.nabil.my.id/cekImeiKemenperin",
          type: "POST",
          dataType: "JSON",
          data: $(this).serialize(),
